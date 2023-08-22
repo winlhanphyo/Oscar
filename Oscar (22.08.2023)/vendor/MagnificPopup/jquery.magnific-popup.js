e8cebfb676e3ca3bc2e2afffb4f3445a432436a7
winlhanphyo <winlhanphyo1@gmail.com>
2023-08-22 09:02:48 +0630

    add design code update

--- Oscar (22.08.2023)/vendor/MagnificPopup/jquery.magnific-popup.js
+++ Oscar (22.08.2023)/vendor/MagnificPopup/jquery.magnific-popup.js
@@ -1571,7 +1571,7 @@ $.magnificPopup.registerModule(IFRAME_NS, {
 	options: {
 		markup: '<div class="mfp-iframe-scaler">'+
 					'<div class="mfp-close"></div>'+
-					'<iframe class="mfp-iframe" src="//about:blank" frameborder="0" allowFullScreen></iframe>'+
+					'<iframe class="mfp-iframe" src="//about:blank" frameborder="0" allowfullscreen></iframe>'+
 				'</div>',
 
 		srcAction: 'iframe_src',
