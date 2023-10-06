ec211d1be5f327925e737351ca340320f0864cb4
winlhanphyo <winlhanphyo1@gmail.com>
2023-10-06 15:18:08 +0630

    category redux.

---
+++ client/src/store/actions/category.action.js
@@ -0,0 +1,38 @@
+import { useSelector } from 'react-redux';
+
+export const persistCategories = () => ({
+  type: "PERSIST_CATEGORIES"
+});
+
+export const addText = value => ({
+  type: "ADD_TEXT",
+  payload: value
+});
+
+export const addCategory = categories => ({
+  type: "ADD_CATEGORY",
+  payload: categories
+});
+
+export const deleteCategory = key => ({
+  type: "DELETE_CATEGORY",
+  payload: key
+});
+
+export const editCategory = key => ({
+  type: "EDIT_CATEGORY",
+  payload: key
+});
+
+export const editAddCategory = obj => ({
+  type: "EDIT_ADD_CATEGORY",
+  payload: obj
+});
+
+export const deleteAll = () => ({
+  type: "DELETE_ALL"
+});
+
+export function useCategory() {
+  return useSelector((state) => state.category.categories)
+};
